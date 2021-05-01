787381e7df13747edea40ad616b846c6b6a5a905
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-01 13:56:17 +0430

    useState --> hard and longly form part 2 of perveios lesson

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -7,14 +7,20 @@ const UseStateObject = () => {
 		message: 'random message',
 	})
 
+	// hard and lonly form of useState
+	const [fName , setFName] = useState('nemo')
+	const [lName , setLName] = useState('mambu')
+	const [message , setMessage] = useState('random message')
+
 	const changeMessage = () =>{
-		setInfo ({...info , message: 'hello'})
+		// setInfo ({...info , message: 'hello'})
+		setMessage('hello')
 	}
   return (
   	<>
-  		<h3>{info.fName}</h3>
-  		<h3>{info.lName}</h3>
-  		<h3>{info.message}</h3>
+  		<h3>{fName}</h3>
+  		<h3>{lName}</h3>
+  		<h3>{message}</h3>
   		<button className='btn' onClick={changeMessage}>
   			change message
   		</button>
