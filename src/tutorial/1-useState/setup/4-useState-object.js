4537499999531ca751a8590155b11b86e3fcdab5
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-01 13:44:09 +0430

    css center flex - setSth to change what we whant {...array} - new corse

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+	const [info , setInfo] = useState({
+		fName:'memo',
+		lName: 'lambu',
+		message: 'random message',
+	})
+
+	const changeMessage = () =>{
+		setInfo ({...info , message: 'hello'})
+	}
+  return (
+  	<>
+  		<h3>{info.fName}</h3>
+  		<h3>{info.lName}</h3>
+  		<h3>{info.message}</h3>
+  		<button className='btn' onClick={changeMessage}>
+  			change message
+  		</button>
+  	</>
+  );
 };
 
 export default UseStateObject;
