c6b391c241f59947b9f96137fde52e7b4210f95b
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-03 17:38:27 +0430

    link users to DOM

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -19,6 +19,20 @@ const UseEffectFetchData = () => {
   return (
   	<>
   		<h3>Github users</h3>
+  		<ul className='users'>
+  			{users.map((user)=>{
+  				const {id,login,avatar_url,html_url} = user;
+  				return(
+  					<li key={id}>
+  						<img src={avatar_url} alt={login} />
+  						<div>
+  							<h4>{login}</h4>
+  							<a href={html_url}>profile</a>
+  						</div>
+  					</li>
+  				)
+  			})}
+  		</ul>
   	</>
   )
 };
