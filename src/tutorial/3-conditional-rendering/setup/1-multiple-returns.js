400fe1fd080a7b0c8e0bc2f7233ed2c5e3224489
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-04 12:38:40 +0430

    if url not found trow error

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,14 +1,23 @@
 import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+const url = 'https://api.github.com/users/QuincyLarsons';
 const MultipleReturns = () => {
-	const [isLoading,setIsLoading] = useState(false);
+	const [isLoading,setIsLoading] = useState(true);
 	const [isError,setIsError] = useState(false);
 	const [user,setUser] = useState('default user')
 
 	useEffect(()=>{
-		setIsLoading(true)
-		fetch(url).then((resp)=>resp.json()).
-		then((user)=> {
+		fetch(url)
+		.then((resp)=>{
+			//if url not found show error
+			if(resp.status >= 200 && resp.status <=299){
+				return resp.json()
+			}
+			else{
+				setIsLoading(false)
+				setIsError(true)
+				throw new Error(resp.statusText);
+			}})
+		.then((user)=> {
 			const {login} = user;
 			setUser(login)
 			setIsLoading(false)
@@ -30,7 +39,11 @@ const MultipleReturns = () => {
 		);
 	}
 
-  return <h2>{user}</h2> 
+  return (
+  	<>
+  	<h2>{user}</h2>
+  	</>
+  ) 
 };
 
 export default MultipleReturns;
