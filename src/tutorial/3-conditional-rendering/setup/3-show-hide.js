cdcca4e8d4bfa384c48abde909eab4f1de33f854
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-05 18:47:02 +0430

    have some eventListener [ ] didnt work

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+	const[show,setShow] = useState(false);
+  return (
+  	<>
+  		<button className='btn' onClick={()=>setShow(!show)}>
+  			toggle compo
+  		</button>
+  		{show && <Component1 />}
+
+  	</>
+  )
+
 };
+
+const Component1 = () => {
+	const [size,setSize] = useState(window.innerWidth);
+	const checkSize = ()=>{
+		setSize(window.innerWidth);
+	}
+	useEffect(()=>{
+		window.addEventListener('resize',checkSize);
+	},[])
+	return (
+		<div style={{marginTop:"35px"
+		}}>
+			<h2>hello I am component</h2>
+			<h4>size:{size}</h4>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing, elit. Ex, voluptatem fugit a id facilis qui saepe facere.</p>
+		</div>
+	)
+}
 
 export default ShowHide;
