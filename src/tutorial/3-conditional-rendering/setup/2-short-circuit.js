c7250b5cdb9255952499a8b524c51c6bede5d6d3
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-05 16:32:43 +0430

    learn && , || , make toggle Error && , || , !

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,22 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+	const [text,setText] = useState('');
+	const [isError,setIsError] = useState(false);
+  	const firstValue = text || 'hello world';
+  	const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+   	<>
+   		<h2>short circuit</h2>
+   		<h1>{firstValue}</h1>
+      <h1>{text || 'john doe'}</h1>
+      <button className='btn' onClick={() => setIsError(!isError)}>
+        toggle Error
+      </button>
+      {isError && <h3>Error...</h3>}
+   	</>
+   )
 };
 
 export default ShortCircuit;
