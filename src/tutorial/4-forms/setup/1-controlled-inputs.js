b2b1fbe1b823dc9b1e2d61689e0f53ed36e17efa
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-18 11:56:00 +0430

    i add onChange for : link what write in input to setemail and setFirstName

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,7 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-	const [firstName,setFirstName] = useState('salam')
+	const [firstName,setFirstName] = useState('')
 	const [email,setEmail] = useState('')
 	const handleSubmit = (e)=>{
 		e.preventDefault();
@@ -18,11 +18,11 @@ const ControlledInputs = () => {
   			<form action="" className="form" onSubmit={handleSubmit}>
   				<div className="form-control">
   					<label htmlFor="firstName">Name :</label>
-  					<input type="text" id='firstName' name='firstName' value={firstName} />
+  					<input type="text" id='firstName' name='firstName' value={firstName} onChange={(e)=>setFirstName(e.target.value)} />
   				</div>
   				<div className="form-control">
   					<label htmlFor="email">Email :</label>
-  					<input type="text" id='email' name='email' />
+  					<input type="text" id='email' name='email' value={email} onChange={(e)=>setEmail(e.target.value)} />
   				</div>
   				<button type="submit">add</button>		
   			</form>
