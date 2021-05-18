98f4702d5ea418ce448e06a3040be17681d2c021
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-18 22:07:21 +0430

    if compelete whole form add person in to people arry and add age to DOM too

--- src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -19,6 +19,11 @@ const ControlledInputs = () => {
 
   const handleSubmit = (e)=>{
     e.preventDefault();
+    if(person.firstName && person.email && person.age){
+      const newPerson = {...person,id:new Date().getTime().toString()}
+      setPeople([...people,newPerson])
+      setPerson({firstName:'',email:'',age:''})
+    }
   }
   return (
     <>
@@ -57,10 +62,11 @@ const ControlledInputs = () => {
           <button type='submit' onClick={handleSubmit} >add person</button>
         </form>
         {people.map((person, index) => {
-          const { id, firstName, email } = person;
+          const { id, firstName, email ,age } = person;
           return (
             <div className='item' key={id}>
               <h4>{firstName}</h4>
+              <p>{age}</p>
               <p>{email}</p>
             </div>
           );
