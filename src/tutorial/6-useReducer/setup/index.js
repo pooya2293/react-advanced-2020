43cd49cd295b35e4341d4dd1d7f7158576443fe6
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-29 23:07:42 +0430

    provide to use reducer

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -2,25 +2,27 @@ import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
-
+const reducer = (state,action)=>{};
+const defaultState = {
+	people: [],
+	isModalOpen: false,
+	modalContent: ''
+}
 const Index = () => {
 	const [name,setName] = useState('')
-	const [people,setPeople] = useState(data)
-	const [showModal,setShowModal] = useState(false)
+	const [state,dispatch] = useReducer(reducer,defaultState)
 
 	const handleSubmit = (e)=>{
 		e.preventDefault()
 		if(name){
-			// setShowModal(true)
-			setPeople([...people,{id:new Date().getTime().toString(), name }])
-			setName('')
+
 		}else{
-			setShowModal(true)
+
 		}
 	}
   return (
   	<>
-  		{showModal && <Modal />}
+  		{state.isModalOpen && <Modal modalContent={state.modalContent} />}
   		<form onSubmit={handleSubmit} className='form'>
   			<div>
   				<input 
@@ -30,7 +32,7 @@ const Index = () => {
   			</div>
   			<button type='submit'>add</button>
   		</form>
-  		{people.map((person)=>{
+  		{state.people.map((person)=>{
   			return (
   				<div key={person.id}>
   					<h4>{person.name}</h4>
