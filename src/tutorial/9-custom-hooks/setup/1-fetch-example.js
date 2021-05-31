41510f503ba7b0a84c4df233d0b04a24fe438343
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-31 12:18:36 +0430

    <new course /> custom-hooks Add useFetch and seporate some function use custom-hooks

--- src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+// import useFetch coustome hook
 import { useFetch } from './2-useFetch'
 
 // ATTENTION!!!!!!!!!!
@@ -6,19 +7,7 @@ import { useFetch } from './2-useFetch'
 const url = 'https://course-api.com/javascript-store-products'
 
 const Example = () => {
-  const [loading, setLoading] = useState(true)
-  const [products, setProducts] = useState([])
-
-  const getProducts = async () => {
-    const response = await fetch(url)
-    const products = await response.json()
-    setProducts(products)
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    getProducts()
-  }, [url])
+  const {loading,products} = useFetch(url);
   console.log(products)
   return (
     <div>
