b16b42da875fe9425cc0e8ace105b00685091159
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-31 13:38:44 +0430

    <new course /> prop-types : dealing with chalenge what happend if one object dont have one item like image url

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ name,image,price }) => {
+  return (
+  	<article className='product'>
+  		<img src={image.url} alt={name} /> {/* getting big fat error becues in url api one an item do'nt have image url and url is undefind too  */}
+    	<h4>{name}</h4>
+    	<p>${price}</p>
+    </article>
+    )
 };
 
 export default Product;
