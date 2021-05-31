873a3acbee3ff4c94ea364c3a37c406b12ff7f83
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-05-31 09:49:14 +0430

    we see how props drilling works : almost its good but when app being bigger defenetly its not good

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -5,28 +5,32 @@ import {data} from '../../../data'
 
 const PropDrilling = () => {
 	const [people,setPeople] = useState(data)
+	const removePerson = (id)=>{
+		setPeople(people.filter((person)=>person.id !== id))
+	}
   return (
   	<section>
   		<h3> prop drilling </h3>
-  		<List people={people} />
+  		<List people={people} removePerson={removePerson} />
   	</section>
   );
 };
 
-const List = ({ people })=>{
+const List = ({ people,removePerson })=>{
 	return(
 		<>
 			{people.map((person)=>{
-				return <SinglePerson key={person.id} />;
+				return <SinglePerson key={person.id} {...person} removePerson={removePerson} />;
 			})}
 		</>
 	);
 }
 
-const SinglePerson = ({ id,name })=>{
+const SinglePerson = ({ id,name,removePerson })=>{
 	return(
 		<div className="item">
-			<h4>single item</h4>
+			<h4>{name}</h4>
+			<button onClick={()=>removePerson(id)}>X</button>
 		</div>
 	);
 }
