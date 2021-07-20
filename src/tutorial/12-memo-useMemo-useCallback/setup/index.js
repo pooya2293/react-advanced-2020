0773f88bba1075b2751fa7a68d29c2dd186485b6
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-07-20 20:31:37 +0430

    adding addToCard and have to props drilling but also with memo again by every click log big and single list

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -9,7 +9,12 @@ const url = 'https://course-api.com/javascript-store-products'
 
 const Index = () => {
   const { products } = useFetch(url)
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [cart,setCart] = useState(0);
+
+  const addToCart =()=>{
+    setCart(cart + 1);
+  } 
 
   return (
     <>
@@ -17,27 +22,30 @@ const Index = () => {
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{marginTop:'3rem'}}>
+        cart:{cart}
+      </h1>
+      <BigList products={products} addToCart={addToCart} />
     </>
   )
 }
 
-const BigList = React.memo(({ products }) => {
+const BigList = React.memo(({ products,addToCart }) => {
   useEffect(() => {
     console.log('big list')
   });
   return (
     <section className='products'>
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return <SingleProduct key={product.id} {...product} addToCart={addToCart}></SingleProduct>
       })}
     </section>
   )
 });
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields,addToCart }) => {
   useEffect(() => {
-    console.log('single list')//evry render by click 12th show this
+    console.count('single list')//evry render by click 12th show this
   });
   let { name, price } = fields
   price = price / 100
@@ -48,6 +56,7 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCart}>add to card</button>
     </article>
   )
 }
